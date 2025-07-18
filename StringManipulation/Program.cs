d4e9f540c7eb0b0609f4d6bbc8a4513217543e88
Pbierley <P.bierley@yahoo.com>
2025-07-18 16:42:11 -0400

    this is literally java with a namespace

---
+++ StringManipulation/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StringAplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name:");
+            String name = Console.ReadLine();
+            Console.WriteLine(" Hi " + name + " Please enter your phone number using - to seperate area code:");
+            String phoneNumber = Console.ReadLine();
+            phoneNumber = phoneNumber.Replace("-", "/");
+            Console.WriteLine("phoneNumber:" + phoneNumber);
+        }
+    }
+}
---
+++ randomNumberGuesser/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MyApplication
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Random random = new Random();
+
+            int randomNum = random.Next(1, 101);
+            Console.WriteLine("Welcome to the number guessing application!");
+            Console.WriteLine("Please guess a number between 1 and 100:");
+            int guess;
+            Boolean guessValid = false;
+            while (!guessValid)
+            {
+                guess = Convert.ToInt32(Console.ReadLine());
+                if (guess == randomNum)
+                {
+                    Console.WriteLine("Congratulations! You guessed the correct number.");
+                    guessValid = true;
+                }
+                else if (guess < randomNum)
+                {
+                    Console.WriteLine("Your guess is too low.");
+                    Console.WriteLine("Please try again:");
+                }
+                else
+                {
+                    Console.WriteLine("Your guess is too high.");
+                    Console.WriteLine("Please try again:");
+                }
+            }
+
+
+        }
+    }
+}
